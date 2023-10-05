095554d7183f0b57217ec6e2cf83c1ed8a13d734
MrMeepsle <mynameismailtje@gmail.com>
2023-10-05 16:54:32 +0200

    shit

--- slurm_job.sh
+++ slurm_job.sh
@@ -13,4 +13,4 @@ module load miniconda3/4.12.0
 conda activate pmf
 
 srun python --version
-srun python main.py --output outputs/5way-5shot-10-query --dataset custom --epoch 100 --arch dino_small_patch16 --device cuda --fp16 --nClsEpisode 5 --nSupport 5 --nQuery 10 --nEpisode 2000 --sched cosine --lr 5e-5 --warmup-lr 1e-6 --min-lr 1e-6 --warmup-epochs 5 --num_workers 10
+srun python main.py --output outputs/5way-5shot-10-query --dataset custom --epoch 100 --arch dino_small_patch16 --device cuda:0 --fp16 --nClsEpisode 5 --nSupport 5 --nQuery 10 --nEpisode 2000 --sched cosine --lr 5e-5 --warmup-lr 1e-6 --min-lr 1e-6 --warmup-epochs 5 --num_workers 10
